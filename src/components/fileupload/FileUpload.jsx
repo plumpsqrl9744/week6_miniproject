2fd024f9b1a56f85159353bbc00b5f64e3de7319
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-08-14 23:04:39 +0900

    UPDATE 0814

--- src/components/fileupload/FileUpload.jsx
+++ src/components/fileupload/FileUpload.jsx
@@ -4,13 +4,19 @@ import Button from 'react-bootstrap/Button';
 
 
 const FileUpload = () => {
-    
+    const [posts, setPosts] = useState([
+        {
+          image : "가",
+          title : "나",
+          content:"다"
+        }
+      ]);
     const [selectedImages, setSelectedImages] = useState([]);
-
+    const [imgSelect , setImgSelect] = useState(false)
     const onSelectFile = (event) => {
         const selectedFiles = event.target.files;
         const selectedFilesArray = Array.from(selectedFiles);
-
+        setImgSelect((prev) => !prev)
         const imagesArray = selectedFilesArray.map((file) => {
             return URL.createObjectURL(file);
         });
@@ -21,28 +27,33 @@ const FileUpload = () => {
         event.target.value = "";
     };
 
-  function deleteHandler(image) {
-    setSelectedImages(selectedImages.filter((e) => e !== image));
-    URL.revokeObjectURL(image);
-  }
-  const ImageUpload = () => {
+    const deleteHandler = (image) => {
+        setSelectedImages(selectedImages.filter((e) => e !== image));
+        URL.revokeObjectURL(image);
+        setImgSelect((prev) => !prev)
+    }
+    const ImageUpload = () => {
     return(
-        <label>
-            + Add images
+        <label className="img-label">
+            {imgSelect ? null 
+                : <div>
+                + Add image
+                </div>}
             <input
             type="file"
             name="images"
             onChange={onSelectFile}
             accept="image/png , image/jpeg, image/webp"
+            className="file-input"
             />
             <div className="images">
                 {selectedImages &&
                 selectedImages.map((image, index) => {
                     return (
                     <div key={image} className="image">
-                        <img src={image} height="400" alt="upload" className="image"/>
+                        <img src={image} height="330" alt="upload" className="image"/>
                         <Button variant="primary" onClick={() => deleteHandler(image)}>
-                        delete
+                            Delete
                         </Button>
                     </div>
                     );
@@ -51,12 +62,11 @@ const FileUpload = () => {
         </label>
         )
     }
-  return (
-    <div className='fileupload'>
-        <ImageUpload></ImageUpload>
-        <Button style={{backgroundColor:'rgb(255, 242, 0)',color:"black", border:"none"}}>Example</Button>
-    </div>
-  )
+    return (
+        <div className='fileupload'>
+            <ImageUpload></ImageUpload>
+        </div>
+    )
 }
 
 export default FileUpload
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,21 +5,61 @@ import "./Post.scss";
 import Form from 'react-bootstrap/Form';
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import FileUpload from "../fileupload/FileUpload" 
+import Button from "react-bootstrap/esm/Button";
 
-function Post() {
-  const [modal, setModal] = useState(false);
+const Post = () => {
+  const [posts, setPosts] = useState([
+    {
+      titles : "나",
+      contents:"다"
+    }
+  ]);
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const onChange = (e) => {
+    e.preventDefault();
+    setTitle(e.target.value)
+  }
+  const onChanges = (e) => {
+    e.preventDefault();
+    setContent(e.target.value)
+  }
+  const handlePost = (e) => {
+    e.preventDefault();
+    setPosts([{...posts,
+      titles:title,
+      contents:content
+    }])
+    console.log(posts)
+  }
+  const UploadContainer = () => {
+    return(
+      <div className="upload-container">
+        <Form.Group className="input-box">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" placeholder="Title" value={title} onChange={onChange}/>
+        </Form.Group>
+        <Form.Group className="input-box">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} placeholder ="Content" value={content} onChange={onChanges}/>
+        </Form.Group>
+      </div>
+    )
+  }
   return (
     <div className="post">
       <Header></Header>
         <Form className="post-form">
-          <Link to="/"><AiOutlineArrowLeft className="arrow-left"></AiOutlineArrowLeft></Link>
-          <div className="desc">
-              Create Card !
+          <div>
+            <Link to="/"><AiOutlineArrowLeft className="arrow-left"></AiOutlineArrowLeft></Link>
+            <div className="desc">
+                Create Card !
+            </div>
           </div>
           <hr></hr>
           <FileUpload></FileUpload>
-            {/* <Button type="submit">Submit</Button> */}
-    
+          <UploadContainer></UploadContainer>
+          <Button onClick={handlePost} type="submit">Submit</Button>
         </Form>
     </div>
   );
